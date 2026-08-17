9e2a5a5e62929b51472a29a60cee6687156fe6fe
D4rkw42 <william.conceicao042@gmail.com>
2026-08-17 09:15:07 -0300

    Pequenos ajustes

--- src/market_insights/utils/math/market/operations.py
+++ src/market_insights/utils/math/market/operations.py
@@ -1,6 +1,16 @@
 import numpy as np
 import numpy.typing as npt
 
+from market_insights.utils.math import ScientificNotation
+
+# Simple Moving Average (SMA)
+
+def SMA(vec: npt.NDArray[np.float64], period: int) -> ScientificNotation:
+    average = np.average(vec[0:period])
+    return ScientificNotation(average)
+
+# Exponential Moving Average (EMA)
+
 # Moving Average Convergence Divergence (MACD)
 
 def MACD() -> npt.NDArray[np.float64]:
